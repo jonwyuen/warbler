4a6f604201b9c6ec276844b1c9652c789bd5ded4
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-05 20:02:40 -0800

    added login route

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,45 @@ router.post("/signup", function(req, res, next) {
     });
 });
 
-router.post("/login", function(req, res, next) {});
+router.post("/login", function(req, res, next) {
+  db.User.findOne({ username: req.body.username }).then(function(user) {
+    user.comparePassword(req.body.password, function(err, isMatch) {
+      if (isMatch) {
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+        res
+          .status(200)
+          .json({
+            token,
+            userId: user.id,
+            username: user.usernmae,
+            profileImage: user.profileImage
+          });
+      }
+    });
+  });
+});
+
+exports.signin = function(req, res) {
+  db.User
+    .findOne({ email: req.body.email })
+    .then(function(user) {
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch) {
+          var token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+          res.status(200).json({
+            userId: user.id,
+            username: user.username,
+            profileImageUrl: user.profileImageUrl,
+            token
+          });
+        } else {
+          res.status(400).json({ message: "Invalid Email/Password." });
+        }
+      });
+    })
+    .catch(function(err) {
+      res.status(400).json({ message: "Invalid Email/Password" });
+    });
+};
 
 module.exports = router;
